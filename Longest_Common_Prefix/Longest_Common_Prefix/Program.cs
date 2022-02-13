4e72ce6d97b21793f8a4f35c74f5119729b9d50b
ShuvaNath69 <shuvanathbd@gmail.com>
2022-02-13 22:46:13 +0600

    commit after solving the problem

---
+++ Longest_Common_Prefix/Longest_Common_Prefix/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace Longest_Common_Prefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = new string[]{ "dog", "racecar", "car" };
+
+            LongestCommonPrefix(strs);
+        }
+
+        static string LongestCommonPrefix(string[] strs)
+        {
+            var getLength = strs.Length;
+
+            if (getLength == 0)
+                return "";
+
+            else if (getLength == 1)
+                return strs[0];
+
+            else
+            {
+                var createString = "";
+                var sortedArray = strs.ToArray();
+                Array.Sort(sortedArray);
+
+                string firstString = sortedArray[0];
+                string lastString = sortedArray[^1];  
+
+                for (int i = 0; i < firstString.Length; i++)
+                {
+                    if (firstString[i] != lastString[i])
+                    {
+                        break;                 
+                    }
+                    else
+                    {
+                        createString += firstString[i];
+                    }
+
+                }
+                return createString;
+            }
+
+            return "";
+            
+        }
+    }
+}
